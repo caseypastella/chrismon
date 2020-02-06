9a7c3ef1c195253f062332ddf5309b9377100a90
Casey Pastella <casey.pastella.15@cnu.edu>
2020-02-05 19:30:05 -0500

    fifth commit

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,17 +1,51 @@
 import React, {Component} from 'react';
-import {NavLink, NavItem} from 'reactstrap';
+import {Container, Media, Button, Row, Col} from 'reactstrap';
 import {LinkContainer} from 'react-router-bootstrap';
+import './Services.css';
 
 
-class Services extends Component {
-    render() {
-        return (
-        <div>
-            <h1>Services</h1>
+
+const Services = (props) => {
+    return (
+        <div> 
+                <Media>
+                    <Media left top>
+                        <Media className = "media1" object src = {process.env.PUBLIC_URL + '/jpeg-10.jpg'} alt = "Placeholder" />
+                    </Media>
+                    <Media body className = "mediabody">
+                        <Media heading className = "mediaheading">
+                            Service 1
+                        </Media>
+                            Service details
+                    </Media>
+                </Media>
+                <Media className = "mt-1">
+                    <Media left middle>
+                        <Media className = "media2" object src = {process.env.PUBLIC_URL + '/jpeg-10.jpg'} alt = "Placeholder" />
+                    </Media>
+                    <Media body className = "mediabody">
+                        <Media heading className = "mediaheading">
+                            Service 1
+                        </Media>
+                            Service details
+                    </Media>
+                </Media>
+                <Media className = "mt-1">
+                    <Media left bottom>
+                        <Media className = "media3" object src = {process.env.PUBLIC_URL + '/jpeg-10.jpg'} alt = "Placeholder" />
+                    </Media>
+                    <Media body className = "mediabody">
+                        <Media heading className = "mediaheading">
+                            Service 1
+                        </Media>
+                            Service details
+                    </Media>
+                </Media>
+                <Button color = "secondary" size= "lg block">
+                    Get an estimate today!
+                </Button>
         </div>
-        );
-    };
-   
+    );
 }
 
 export default Services; 
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const config = require('config');
 
 const app = express();
 
+
+
 // Bodyparser Middleware
 app.use(express.json());
 
